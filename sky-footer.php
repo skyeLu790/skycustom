b9833d225471364b40d0b52c343d832d896a9885
Skye Lu <wen.lu1@my.jcu.edu.au>
2020-05-28 21:41:31 +1000

    plugin descripte

---
+++ sky-footer.php
@@ -0,0 +1,27 @@
+/**
+* Plugin Name: Insert Headers and Footers
+* Plugin URI: http://www.wpbeginner.com/
+* Version: 1.4.5
+* Author: WPBeginner
+* Author URI: http://www.wpbeginner.com/
+* Description: Allows you to insert code or text in the header or footer of your WordPress blog
+* License: GPL2
+* Text Domain: insert-headers-and-footers
+* Domain Path: languages
+*/
+
+/*  Copyright 2019 WPBeginner
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
